609a145dfbe75a7792b3993680b0b4413c299c91
Yosriko Rahmat <yosriku.rahmat@ti.ukdw.ac.id>
2024-02-11 11:51:17 +0700

    Revert "Revert "Menambah atribut prioritas""
    This reverts commit e25fd0cc75fabd715072af0266d773cbbf21a633.

--- tasks.sql
+++ tasks.sql
@@ -31,22 +31,23 @@ CREATE TABLE `tasks` (
                          `kode_task` int(11) NOT NULL,
                          `judul` varchar(50) NOT NULL,
                          `deskripsi` text NOT NULL,
-                         `tanggal` varchar(10) DEFAULT NULL
+                         `tanggal` varchar(10) NOT NULL,
+                        `prioritas` ENUM('low','medium','high') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`kode_task`, `judul`, `deskripsi`, `tanggal`) VALUES
-                                                                       (10, 'coba', 'cobalah', '25-02-2024'),
-                                                                       (12, 'minum', 'minum air', '13-02-2024'),
-                                                                       (13, 'makan', 'makan nasi', '16-02-2024'),
-                                                                       (14, 'belajar', 'belajar kotlin', '17-02-2024'),
-                                                                       (15, 'kelas kotlin', 'belajar membuat program kotlin', '21-02-2024'),
-                                                                       (16, 'ngeFiks Bug', 'memperbaiki bug yang muncul', '23-02-2024'),
-                                                                       (17, 'TaskProjeck', 'Mengerjakan projeck sederhana', '24-02-2024'),
-                                                                       (18, 'mandi pakai sabun', 'biar wangi', '26-02-2024');
+INSERT INTO `tasks` (`kode_task`, `judul`, `deskripsi`, `tanggal`,`prioritas`) VALUES
+                                                                       (10, 'coba', 'cobalah', '25-02-2024','low'),
+                                                                       (12, 'minum', 'minum air', '13-02-2024','low'),
+                                                                       (13, 'makan', 'makan nasi', '16-02-2024','low'),
+                                                                       (14, 'belajar', 'belajar kotlin', '17-02-2024','medium'),
+                                                                       (15, 'kelas kotlin', 'belajar membuat program kotlin', '21-02-2024','medium'),
+                                                                       (16, 'ngeFiks Bug', 'memperbaiki bug yang muncul', '23-02-2024','high'),
+                                                                       (17, 'TaskProjeck', 'Mengerjakan projeck sederhana', '24-02-2024','high'),
+                                                                       (18, 'mandi pakai sabun', 'biar wangi', '26-02-2024','low');
 
 --
 -- Indexes for dumped tables
